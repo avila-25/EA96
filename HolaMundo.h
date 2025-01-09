a0136fe149928c01124ebc95369df8e0f4f02357
avila-25 <avila.aguilar2509@gmail.com>
2025-01-08 20:38:33 -0600

    creación de programa de generar tablas

---
+++ HolaMundo.h
@@ -0,0 +1,8 @@
+//
+// Created by usuario on 07/01/2025.
+//
+
+#ifndef HOLAMUNDO_H
+#define HOLAMUNDO_H
+
+#endif //HOLAMUNDO_H
---
+++ Variables.c
@@ -0,0 +1,14 @@
+//
+// Created by usuario on 07/01/2025.
+//
+#include <stdio.h>
+
+int main() {
+    char nombre[100];
+
+    printf("Ingresa tu nombre:");
+    scanf("%s", &nombre);
+    for (int j = 0; j < 5; j++) {
+        printf("*");
+    }
+}
